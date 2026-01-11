cf4ae472b3d8dddf28bbbd0948a65a6e47282d3e
hema-2407 <hemadurai490@gmail.com>
2026-01-11 15:53:38 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    String name;
+    int marks;
+
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter student name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Enter marks: ");
+            int marks = sc.nextInt();
+            sc.nextLine();
+
+            students.add(new Student(name, marks));
+        }
+
+        int total = 0;
+        int highest = students.get(0).marks;
+        int lowest = students.get(0).marks;
+
+        for (Student s : students) {
+            total += s.marks;
+            if (s.marks > highest) highest = s.marks;
+            if (s.marks < lowest) lowest = s.marks;
+        }
+
+        double average = (double) total / students.size();
+
+        System.out.println("\n--- Student Grade Summary ---");
+        System.out.println("Average Marks: " + average);
+        System.out.println("Highest Marks: " + highest);
+        System.out.println("Lowest Marks: " + lowest);
+
+        sc.close();
+    }
+}
